35304d2829e75e9d49f32f60ab08e3b2ab868482
alcodinabr <ide41@hotmail.com>
2018-11-28 19:51:34 +0100

    Segon commit del projecte mitjana

--- mitjana.c
+++ mitjana.c
@@ -13,8 +13,8 @@ int main() {
 		suma=suma+nou_valor;
 		numero_repeticions=numero_repeticions+1;
 		printf("Vols continuar (s/n): ");
-		scanf(" %c",&consulta) 
-	}
+		scanf(" %c",&consulta);
+        }
 	mitjana=suma/numero_repeticions;
 	printf("La mitjana resultant és: %.2f\n",mitjana);	
 	return 0;
